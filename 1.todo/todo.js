545a284b749c9d1d0af8d7f4f8c7045584e760ea
hachi-ops <elen20@protonmail.com>
2021-08-20 15:37:23 +0100

    1.comments 2.added functionality o prevent creating empty list items and clear user  previous input

--- 1.todo/todo.js
+++ 1.todo/todo.js
@@ -1,7 +1,8 @@
 function todoList() {
-    var item = document.getElementById("todoInput").value;
-    var text = document.createTextNode(item);
-    var newItem = document.createElement("li");
-    newItem.appendChild(text);
-    document.getElementById("todoList").appendChild(newItem);
+    var item = document.getElementById("todoInput").value; // fetches value of the user input
+    var text = document.createTextNode(item); // transforms user's input into text node
+    var newItem = document.createElement("li"); // creates new li element in a list
+    newItem.appendChild(text); // appends newly created text node with the user's input value into a newly created list item
+    document.getElementById("todoList").appendChild(newItem); // appends the above new item into the todo list
+    todoInput.value = ""; // clears up last user's input after click and prevents adding empty list items upon click
 }
