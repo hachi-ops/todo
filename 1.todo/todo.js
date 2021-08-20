46d63d37be3bfc1242923845dc757b37cc071959
hachi-ops <elen20@protonmail.com>
2021-08-20 17:48:00 +0100

    cross out item on click

--- 1.todo/todo.js
+++ 1.todo/todo.js
@@ -5,4 +5,20 @@ function todoList() {
     newItem.appendChild(text); // appends newly created text node with the user's input value into a newly created list item
     document.getElementById("todoList").appendChild(newItem); // appends the above new item into the todo list
     todoInput.value = ""; // clears up last user's input after click and prevents adding empty list items upon click
+
+
+    newItem.addEventListener('click', function() {
+        newItem.style.textDecoration = "line-through"; // crosses out item on click
+    })
+
+
+
+    // newItem.addEventListener('dblclick', function() {
+    //     newItem.remove(); // removes list item on dbl click
+
+    //     // Below code does the same as <newItem.remove()> but is more verbose:
+
+    //     // document.getElementById("todoList").removeChild(newItem);
+    // })
+
 }
